225522a3ff8875605e332055a583829d8c7bbb7c
Godwin Precious <godwinprecious47@gmail.com>
2021-05-03 12:16:25 +0100

    fixed hdf5 path

--- dog_vs_cat/configs/dog_vs_cat_configs.py
+++ dog_vs_cat/configs/dog_vs_cat_configs.py
@@ -12,7 +12,7 @@ VAL_SPLIT = 0.20
 
 HDF5_BUFFER_SIZE = 1000
 
-TRAIN_HDF5 = "../datasets/kaggle_dog_vs_cat/hdf/train.hdf5"
+TRAIN_HDF5 = "../datasets/kaggle_dog_vs_cat/hdf5/train.hdf5"
 TEST_HDF5 = "../datasets/kaggle_dog_vs_cat/hdf5/test.hdf5"
 VAL_HDF5 = "../datasets/kaggle_dog_vs_cat/hdf5/val.hdf5"
 
