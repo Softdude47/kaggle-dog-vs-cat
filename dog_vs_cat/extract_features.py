0e1c288d32436ac39d7db61cc5403914683ec580
Godwin Precious <godwinprecious47@gmail.com>
2021-06-12 23:21:13 +0100

    changed 'keras' to 'tensorflow.keras'

--- dog_vs_cat/extract_features.py
+++ dog_vs_cat/extract_features.py
@@ -5,10 +5,10 @@ import numpy as np
 from random import shuffle
 from imutils.paths import list_images
 from sklearn.preprocessing import LabelEncoder
-from keras.applications import ResNet50
-from keras.applications import imagenet_utils
-from keras.preprocessing.image import load_img
-from keras.preprocessing.image import img_to_array
+from tensorflow.keras.applications import ResNet50
+from tensorflow.keras.applications import imagenet_utils
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras.preprocessing.image import img_to_array
 
 sys.path.append("../")
 from dog_vs_cat.configs import dog_vs_cat_configs as configs
@@ -52,7 +52,7 @@ shuffle(train_path)
 # extract and encode image labels
 le = LabelEncoder()
 train_labels = [configs.get_label(path) for path in train_path]
-train_labels = le.fit(train_labels)
+train_labels = le.fit_transform(train_labels)
 
 # initialize hdf5 database class and add class names (string format) of images
 db = HDF5DatasetWriter(args["output"], feature_ref_name="data", buffer_size=args["buffer_size"], shape=(len(train_path), 7 * 7 * 2048))
